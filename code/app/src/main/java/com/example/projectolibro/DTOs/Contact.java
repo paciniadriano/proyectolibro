7636c567245e99c37c443ad49a9b9c5b96059d63
Cristian Santilli <csantilli@southplans.com.ar>
2019-01-22 21:17:21 -0300

    Retrieving contacts.

---
+++ code/app/src/main/java/com/example/projectolibro/DTOs/Contact.java
@@ -0,0 +1,37 @@
+package com.example.projectolibro.DTOs;
+
+public class Contact {
+    String id;
+    String name;
+    String phoneNumber;
+
+    public Contact(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- code/app/src/main/java/com/example/projectolibro/FriendsActivity.java
+++ code/app/src/main/java/com/example/projectolibro/FriendsActivity.java
@@ -1,13 +1,71 @@
 package com.example.projectolibro;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 
+import com.example.projectolibro.DTOs.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendsActivity extends AppCompatActivity {
+
+    private boolean isContactsAccessAllowed() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestContactAccessPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 23);
+    }
+
+    private List<Contact> getContactList() {
+        List<Contact> contacts = new ArrayList<>();
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contacts.add(new Contact(id, name, phoneNo));
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if(cur!=null){
+            cur.close();
+        }
+
+        return contacts;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        if (!isContactsAccessAllowed()) {
+            requestContactAccessPermission();
+        }
+        List<Contact> contacts = getContactList();
+
     }
 }
